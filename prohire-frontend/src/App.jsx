c0c98fcd7e048802c3e8608c95cc35fb22547be5
PythonicDG <dipakgaikwadmg@gmail.com>
2025-08-09 19:51:00 +0530

    [fix]

--- prohire-frontend/src/App.jsx
+++ prohire-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage";
-import RegistrationForm from "./pages/RegistrationForm";
+import RegistrationForm from "./pages/registrationform";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
